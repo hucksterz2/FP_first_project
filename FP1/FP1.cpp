52258395a0b2cc4a54972ee7c969ea7f34d96a15
hucksterz2 <bragin.aleksey06.03.07@gmail.com>
2026-04-27 16:30:22 +0700

    added Node struct and some functions

--- FP1/FP1.cpp
+++ FP1/FP1.cpp
@@ -1,1 +1,55 @@
-﻿
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <functional>
+using namespace std;
+
+struct Order {
+	int id;
+	int orderAmount;
+	string categorty;
+	string date;
+};
+
+struct Node {
+	Order data;
+	Node* next;
+};
+
+function<void(Node*&, const Order&)> append = [&](Node*& head, const Order& o) -> void {
+	(head == nullptr)
+		? (head = new Node{ o, nullptr }, void())
+		: append(head->next, o);
+	};
+
+auto parseOrder = [](const string& line) -> Order {
+	Order o;
+	stringstream ss(line);
+	string token;
+
+	getline(ss, token, ','); o.id = stoi(token);
+	getline(ss, token, ','); o.orderAmount = stoi(token);
+	getline(ss, token, ','); o.categorty = token.substr(1);
+	getline(ss, token, ','); o.date = token.substr(1);
+
+	return o;
+	};
+
+function<void(Node*)> printList = [&](Node* node) -> void {
+	(node == nullptr)
+		? void()
+		: (cout << node->data.id << ", "
+			<< node->data.orderAmount << ", "
+			<< node->data.categorty << ", "
+			<< node->data.date << "\n",
+			printList(node->next));
+	};
+
+function < Node* (Node*, function < bool(const Order&)>)> filter =
+[&](Node* node, function<bool(const Order&)> pred) -> Node* {
+	return (node == nullptr)
+		? nullptr
+		: (pred(node->data)
+			? new Node{ node->data, filter(node->next, pred) }
+			: filter(node->next, pred));
+			};
