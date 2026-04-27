aac300a31593b437af84eef11d34cb179dcded95
hucksterz2 <bragin.aleksey06.03.07@gmail.com>
2026-04-27 17:18:43 +0700

    added function, ended prepare to main

--- FP1/FP1.cpp
+++ FP1/FP1.cpp
@@ -16,10 +16,22 @@ struct Node {
 	Node* next;
 };
 
+struct StrNode {
+	string data;
+	StrNode* next;
+};
+
 function<void(Node*&, const Order&)> append = [&](Node*& head, const Order& o) -> void {
 	(head == nullptr)
 		? (head = new Node{ o, nullptr }, void())
 		: append(head->next, o);
+	};
+
+function<void(StrNode*&, const string&)> appendStr =
+[&](StrNode*& head, const string& s) -> void {
+	(head == nullptr)
+		? (head = new StrNode{ s, nullptr }, void())
+		: appendStr(head->next, s);
 	};
 
 auto parseOrder = [](const string& line) -> Order {
@@ -53,3 +65,71 @@ function < Node* (Node*, function < bool(const Order&)>)> filter =
 			? new Node{ node->data, filter(node->next, pred) }
 			: filter(node->next, pred));
 			};
+
+auto makeDataFilter = [](const string& from, const string& to) {
+	return [from, to](const Order& o) -> bool {
+		return (o.date >= from && o.date <= to) ? true : false;
+		};
+	};
+
+auto makePositiveFilter = []() {
+	return[](const Order& o) -> bool {
+		return o.orderAmount > 0 ? true : false;
+		};
+	};
+
+auto makeCategoryFilter = [](StrNode* catList) {
+	return [catList](const Order& o) -> bool {
+		function<bool(StrNode*)> contains = [&](StrNode* node) -> bool {
+			return (node == nullptr)
+				? false
+				: (node->data == o.categorty
+					? true
+					: contains(node->next));
+			};
+		return contains(catList);
+		};
+	};
+
+function<int(Node*)> calcTotal = [&](Node* node) -> int {
+	return (node == nullptr)
+		? 0
+		: node->data.orderAmount + calcTotal(node->next);
+	};
+
+function <Order(Node*, Order)> calcMax = [&](Node* node, Order cur) -> Order {
+	return (node == nullptr)
+		? cur
+		: calcMax(node->next,
+			(node->data.orderAmount > cur.orderAmount
+				? node->data
+				: cur));
+	};
+
+function<int(Node*, const string&)> calcSumByCategory =
+[&](Node* node, const string& cat) -> int {
+	return (node == nullptr)
+		? 0
+		: (node->data.categorty == cat
+			? node->data.orderAmount + calcSumByCategory(node->next, cat)
+			: calcSumByCategory(node->next, cat));
+	};
+
+function<int(Node*, const string&)> calcCountByCategory =
+[&](Node* node, const string& cat) -> int {
+	return (node == nullptr)
+		? 0
+		: (node->data.categorty == cat
+			? 1 + calcCountByCategory(node->next, cat)
+			: calcCountByCategory(node->next, cat));
+	};
+
+function<void(Node*, StrNode*)> printAvgByCategory =
+[&](Node* orders, StrNode* catNode) -> void {
+	(catNode == nullptr)
+		? void()
+		: (cout << catNode->data << ": "
+			<< calcSumByCategory(orders, catNode->data) /
+			calcCountByCategory(orders, catNode->data) << "\n",
+			printAvgByCategory(orders, catNode->next));
+	};
